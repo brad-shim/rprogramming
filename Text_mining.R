5b96a627a7d2e44fbb7bcfc4dc0a1abbd7a5cdab
brad.shim <brad.shim@gmail.com>
2022-09-13 16:21:28 +0900

    Text mining example

---
+++ Text_mining.R
@@ -0,0 +1,30 @@
+# 
+install.packages("dplyr")
+library(dplyr)
+
+# Quantitative Analysis of Textual Data
+install.packages("quanteda")
+library("quanteda")
+
+#
+install.packages("tidytext")
+library("tidytext")
+
+#
+install.packages("tibble")
+library("tibble")
+
+#
+install.packages("tm")
+library("tm")
+
+tribble(~id, ~name, ~price,
+        "A001", "Mouse", 30000)
+
+tribble(~id, ~name, ~price,
+        "A001", "Mouse", 30000,
+        "A002", "Keyboard", 50000,
+        "A003", "Monitor", 150000)
+
+
+
